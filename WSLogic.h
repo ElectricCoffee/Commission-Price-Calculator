3bf1cbfd4439bfa0b9ac3fc65ff43ba82c3261b6
ElectricCoffee <slench102+git@gmail.com>
2013-09-16 11:42:39 +0200

    Added the logic class files

---
+++ WSLogic.h
@@ -0,0 +1,15 @@
+//
+//  WSLogic.h
+//  Commission Price Calculator
+//
+//  Created by Electric Coffee on 9/16/13.
+//  Copyright (c) 2013 Electric Coffee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WSLogic : NSObject
+
+
+
+@end
---
+++ WSLogic.m
@@ -0,0 +1,13 @@
+//
+//  WSLogic.m
+//  Commission Price Calculator
+//
+//  Created by Electric Coffee on 9/16/13.
+//  Copyright (c) 2013 Electric Coffee. All rights reserved.
+//
+
+#import "WSLogic.h"
+
+@implementation WSLogic
+
+@end
